d52f3442e345eb8743ce52053243e1afdf219100
Nick Wing <nwing@txstate.edu>
2021-10-09 08:49:55 -0500

    upgraded ESM export strategy

---
+++ dist-esm/index.js
@@ -0,0 +1,12 @@
+import pkg from '../dist/index.js'
+
+export const AuthError = pkg.AuthError
+export const AuthorizedService = pkg.AuthorizedService
+export const BaseService = pkg.BaseService
+export const Context = pkg.Context
+export const GQLServer = pkg.GQLServer
+export const MutationMessage = pkg.MutationMessage
+export const MutationMessageType = pkg.MutationMessageType
+export const ResolveReference = pkg.ResolveReference
+export const UnimplementedError = pkg.UnimplementedError
+export const ValidatedResponse = pkg.ValidatedResponse
