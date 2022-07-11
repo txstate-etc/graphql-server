0733b5e5481edc5fc708ce4e6312912a37a18d9d
Charles Tabor <ct37@txstate.edu>
2022-07-11 15:31:27 -0500

    Clone objects before removing fields. Make mayview optional.

--- src/service.ts
+++ src/service.ts
@@ -1,4 +1,4 @@
-import { filterAsync } from 'txstate-utils'
+import { clone, filterAsync } from 'txstate-utils'
 import { Context, Type } from './context'
 
 export abstract class BaseService<AuthType = any> {
@@ -25,6 +25,10 @@ export abstract class BaseService<AuthType = any> {
   }
 }
 
+/*
+  * This service should not mutate any incoming objects; so will make a copy of
+  * the objects before passing them to the removeProperties method.
+  */
 export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedType = ObjType> extends BaseService<AuthType> {
   async removeUnauthorized (object: ObjType|undefined): Promise<RedactedType|ObjType|undefined>
   async removeUnauthorized (objects: ObjType[]): Promise<RedactedType[]|ObjType[]>
@@ -32,9 +36,9 @@ export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedT
     if (objects == null) return undefined
     if (Array.isArray(objects)) {
       const visible = await filterAsync(objects, async obj => await this.mayView(obj))
-      return await Promise.all(visible.map(async obj => await this.removeProperties(obj))) as RedactedType[]|ObjType[]
+      return await Promise.all(visible.map(async obj => await this.removeProperties(clone(obj)))) as RedactedType[]|ObjType[]
     }
-    if (await this.mayView(objects)) return await this.removeProperties(objects)
+    if (await this.mayView(objects)) return await this.removeProperties(clone(objects))
   }
 
   /**
@@ -42,12 +46,19 @@ export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedT
    * from unauthorized users. For example, a User record might be visible to everyone
    * for directory purposes, but User.socialSecurityNumber needs to be removed
    * for all but the most privileged viewers.
-   *
-   * This method should not mutate the incoming object; return a new object instead.
    */
   protected async removeProperties (object: ObjType): Promise<RedactedType|ObjType> {
     return object as unknown as ObjType
   }
 
-  abstract mayView (obj: ObjType): Promise<boolean>
+  /**
+   * Override this method for any services that need to filter the entire object
+   * from unauthorized users. For example an Address record may only be visible
+   * under a certain context where user is looking at their own address. Returning
+   * a false would filter out the address object so that an undefined would be
+   * returned or the object would be remove from lists. 
+   */
+  protected async mayView (obj: ObjType): Promise<boolean> {
+    return true
+  }
 }
