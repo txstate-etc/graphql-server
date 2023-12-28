d15c74f0ff15af4a14ee542d552a3df76faffaf0
Nick Wing <nwing@txstate.edu>
2023-12-28 09:43:21 -0600

    add a synchronous version of AuthorizedService, release 2.2.2

--- src/service.ts
+++ src/service.ts
@@ -25,10 +25,10 @@ export abstract class BaseService<AuthType = any> {
   }
 }
 
-/*
-  * This service should not mutate any incoming objects; so will make a copy of
-  * the objects before passing them to the removeProperties method.
-  */
+/**
+ * Use this as a base class for your service to add a removeUnauthorized method that can
+ * help you filter out objects the current user isn't allowed to see.
+ */
 export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedType = ObjType> extends BaseService<AuthType> {
   async removeUnauthorized (object: ObjType | undefined): Promise<RedactedType | ObjType | undefined>
   async removeUnauthorized (objects: ObjType[]): Promise<RedactedType[] | ObjType[]>
@@ -49,12 +49,12 @@ export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedT
    *
    * Do NOT mutate the object given, it will be cached in various dataloaders and you
    * don't want to alter the cache. Return a new cloned object instead. You may find
-   * the txstate-utils functions clone, pick, and omit especially helpful.
+   * the txstate-utils functions clone, pick, and omit functions especially helpful.
    *
    * Removing foreign key info in this function can be problematic.
    */
   protected async removeProperties (object: ObjType): Promise<RedactedType | ObjType> {
-    return object as unknown as ObjType
+    return object
   }
 
   /**
@@ -65,6 +65,52 @@ export abstract class AuthorizedService<AuthType = any, ObjType = any, RedactedT
    * returned or the object would be remove from lists.
    */
   protected async mayView (obj: ObjType): Promise<boolean> {
+    return true
+  }
+}
+
+/**
+ * This class is the same idea as AuthorizedService but it expects you to have everything you
+ * need to authorize release of an object already loaded, so that you will not have to make any
+ * async calls. If you can do that, it will greatly improve performance as you will not have to make
+ * several new promises per array element, which is rather expensive.
+ */
+export abstract class AuthorizedServiceSync<AuthType = any, ObjType = any, RedactedType = ObjType> extends BaseService<AuthType> {
+  removeUnauthorized (object: ObjType | undefined): RedactedType | ObjType | undefined
+  removeUnauthorized (objects: ObjType[]): RedactedType[] | ObjType[]
+  removeUnauthorized (objects: ObjType[] | ObjType | undefined) {
+    if (objects == null) return undefined
+    if (Array.isArray(objects)) {
+      const visible = objects.filter(obj => obj != null && this.mayView(obj))
+      return visible.map(obj => this.removeProperties(obj)) as RedactedType[] | ObjType[]
+    }
+    if (this.mayView(objects)) return this.removeProperties(objects)
+  }
+
+  /**
+   * Override this method for any services that need to hide certain properties
+   * from unauthorized users. For example, a User record might be visible to everyone
+   * for directory purposes, but User.socialSecurityNumber needs to be removed
+   * for all but the most privileged viewers.
+   *
+   * Do NOT mutate the object given, it will be cached in various dataloaders and you
+   * don't want to alter the cache. Return a new cloned object instead. You may find
+   * the txstate-utils functions clone, pick, and omit especially helpful.
+   *
+   * Removing foreign key info in this function can be problematic.
+   */
+  protected removeProperties (object: ObjType): RedactedType | ObjType {
+    return object
+  }
+
+  /**
+   * Override this method for any services that need to filter the entire object
+   * from unauthorized users. For example an Address record may only be visible
+   * under a certain context where user is looking at their own address. Returning
+   * a false would filter out the address object so that an undefined would be
+   * returned or the object would be remove from lists.
+   */
+  protected mayView (obj: ObjType): boolean {
     return true
   }
 }
